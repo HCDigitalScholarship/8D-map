8d859bf0b20f9ce50d7c53ff21049bf8df02340a
root <apjanco@gmail.com>
2018-05-17 10:40:15 +0000

    basic search working

--- map_app/views.py
+++ map_app/views.py
@@ -1,7 +1,9 @@
+from dal import autocomplete
 from django.shortcuts import render
 from map_app.models import *
 from map_app.forms import *
 from django.shortcuts import get_object_or_404
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 # Create your views here.
 
@@ -9,8 +11,10 @@ def index(request):
 	if request.method == 'POST':
 		form = SearchForm(request.POST)
 		query = request.POST.get('search', None)
+		print(request.POST)
 		if form.is_valid():
-			sites = PartnerSite.objects.filter(description__icontains=query)
+			sites = PartnerSite.objects.annotate(search=SearchVector('name','area_of_interest__name')).filter(search=query)
+			#sites = PartnerSite.objects.filter(description__icontains=query, name__icontains=query, area_of_interest__name=query)
 			context  = {'sites':sites, 'form':form}
 			return render(request, 'index.html', context)
 		else:
@@ -24,4 +28,16 @@ def index(request):
 def site(request, site):
 	site = get_object_or_404(PartnerSite, name=site)
 	return render(request, 'site.html', {'site':site})
+
+
+
+class SiteAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        # Don't forget to filter out results depending on the visitor !
+        qs = PartnerSite.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
 
